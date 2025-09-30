e93052cd80575c08ddda257e19a260ca4dec98c4
salva66 <henrymkama@gmail.com>
2025-09-30 17:40:47 +0300

    bidhaa_updated_at field modification

--- shopapp/models.py
+++ shopapp/models.py
@@ -51,7 +51,8 @@ class Bidhaas(models.Model):
     brand=models.CharField(max_length=255)
     price=models.DecimalField(max_digits=10, decimal_places=2)
     created_at=models.DateTimeField(auto_now_add=True)
-    updated_at=models.DateTimeField(auto_now_add=True)
+    #updated_at=models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     objects = models.Manager()
 
 class LeaveReportStaff(models.Model):
